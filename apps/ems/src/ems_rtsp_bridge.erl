1bfb5959c47b5f3aabe2ff349456463051519c39
Trent Clarke <trent.clarke@gmail.com>
2011-09-22 23:25:27 +0000

    ems: oops - missed this on last commit

---
+++ apps/ems/src/ems_rtsp_bridge.erl
@@ -0,0 +1,33 @@
+-module (ems_rtsp_bridge).
+-export([handle_request/4]).
+-include("rtsp.hrl").
+
+handle_request(Conn, Request, Headers, Body) ->
+	{Method, _Uri, Sequence, _, _} = rtsp:get_request_info(Request, Headers),
+  handle_request(Conn, Method, Sequence, Request, Headers, Body).
+
+%% ----------------------------------------------------------------------------
+%% @spec handle_request(Method, Request,Headers,Body,State) -> Result
+%%       Method = options | accounce | setup | play | teardown
+%% @end
+%% ----------------------------------------------------------------------------  
+handle_request(Conn, options, Sequence, _, _, _) ->
+  PublicOptions = [?RTSP_METHOD_ANNOUNCE,
+                   ?RTSP_METHOD_DESCRIBE,
+                   ?RTSP_METHOD_SETUP,
+                   ?RTSP_METHOD_PLAY,
+                   ?RTSP_METHOD_PAUSE,
+                   ?RTSP_METHOD_TEARDOWN,
+                   ?RTSP_METHOD_RECORD],
+  Headers = [{"Public", string:join(PublicOptions, ", ")}],
+  rtsp_connection:send_response(Conn, Sequence, ok, Headers, << >>);
+
+%handle_request(Conn, describe, Request, Headers, Body) -> 
+%	Uri = Request#rtsp_request.uri,
+%	{_,_,_,Path} = url:parse(Uri),
+%	Config = get_config_handle(Conn),
+%	MountPoint = case ems_config:get_mount_point(Config, Path) of 
+
+handle_request(Conn, _Method, Sequence, _Request, _Headers, _Body) ->
+  rtsp_connection:send_response(Conn, Sequence, not_implemented, [], << >>).
+
